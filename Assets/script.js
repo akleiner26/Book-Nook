c36fb5c9c6a43a34750db31bfaa84de20c83a172
orangeblack13 <AndrewJKleiner@gmail.com>
2020-06-18 13:44:56 -0400

    gitignore added, api keys added

--- Assets/script.js
+++ Assets/script.js
@@ -9,9 +9,19 @@ var movieCard= $("#movieCard");
 var bookCard =  $("#bookCard");
 // var movieArr = (!savedMovies) ? [] : JSON.parse(savedMovies);
 var movieAPI = "f9bb3ed8";
-var bookAPI = "";
+var bookAPI = "HlkvA90diPLiYVjDMkKAw";
 var movieTitle= "";
 var bookTitle= "";
 var movieGenre= "";
 var bookGenre= "";
 /////////////////////////////////////////////
+
+// Event listener for search button and anon function to set movie title input to variable
+
+// AJAX method to get movie data, including genre
+
+    // Display movie image and other info
+
+    // AJAX method to get book data
+        // Randomly select book title from genre category
+        // Display book image and other info
