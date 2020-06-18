f3d56ca972b17b68bda38e9b3d9c6e8dc6a02a9d
Susan Holland <segh@fastmail.com>
2020-06-18 13:40:28 -0400

    Made api.js

---
+++ Assets/api.js
@@ -0,0 +1,9 @@
+// Event listener for search button and anon function to set movie title input to variable
+
+// AJAX method to get movie data, including genre
+
+    // Display movie image and other info
+
+    // AJAX method to get book data
+        // Randomly select book title from genre category
+        // Display book image and other info
