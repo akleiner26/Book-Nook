3ed102e74a26a7ea79e4d46e63956d8b69f3bafa
Susan Holland <segh@fastmail.com>
2020-06-18 12:44:52 -0400

    Pseudocode

---
+++ Assets/script.js
@@ -0,0 +1,11 @@
+// Assignments
+
+// Event listener for search button and anon function to set movie title input to variable
+
+// AJAX method to get movie data, including genre
+
+    // Display movie image and other info
+
+    // AJAX method to get book data
+        // Randomly select book title from genre category
+        // Display book image and other info
