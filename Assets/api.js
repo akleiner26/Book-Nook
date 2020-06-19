0274f4daeedc355e823e41d30596a58f951769b3
Susan Holland <segh@fastmail.com>
2020-06-19 13:32:13 -0400

    AJAX functions

--- Assets/api.js
+++ Assets/api.js
@@ -1,9 +1,36 @@
-// Event listener for search button and anon function to set movie title input to variable
-
 // AJAX method to get movie data, including genre
 
-    // Display movie image and other info
+function getData(movieTitle) {
+    var movieURL = "https://www.omdbapi.com/?t=" + movieTitle + "&apikey=" + movieAPI;
 
-    // AJAX method to get book data
-        // Randomly select book title from genre category
-        // Display book image and other info
+    $.ajax({
+        url: movieURL,
+        method: "GET"
+    }).then((movieData) => {
+        // Grab movieGenre to pass to book API
+        var movieGenre = movieData.Genre;
+        movieGenre = movieGenre.replace(/(, )/g, "+");
+
+        // Display movie data
+        renderMovies(movieData);
+
+        // AJAX method to get book data
+        var bookLimit = 50;
+
+        $.ajax({
+            url: "https://itunes.apple.com/search?term=" + movieGenre + "&entity=ebook&limit=" + bookLimit,
+            method: "GET"
+        }).then((bookData) => {
+            bookData = JSON.parse(bookData);
+            var bookDataArray = bookData.results;
+
+            // Randomly select number between 0 and bookData array length
+            var randomNum = Math.floor(Math.random() * bookLimit);
+            var bookPickData = bookDataArray[randomNum];
+
+            // Display book image and other info
+            renderBooks(bookPickData);
+
+        });
+    });
+}
